6156ee3f36799fc3cd8fa5b8dda9ef39e5aa2a11
shaheer699 <shaheerjan070@gmail.com>
2026-02-15 14:42:32 +0430

    update app

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.title("Welcome Shaheer 👋")
 name = st.text_input("Enter your name")
 
 if name:
-  ' st.write(f"Welcome {name} ❤️")
+ st.write(f"Welcome {name} ❤️")
 
 st.set_page_config(page_title="Shaheer App", page_icon="🔥", layout="centered")
